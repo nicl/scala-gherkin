d48907d16fdafc697927f993b383593f442b2bfc
Nicolas Long <nicolaslong@gmail.com>
2013-07-28 18:45:28 +0100

    Add initial parser

---
+++ src/main/scala/net/room271/GherkinParser.scala
@@ -0,0 +1,26 @@
+package net.room271
+
+import scala.util.parsing.combinator.RegexParsers
+
+class GherkinParser extends RegexParsers {
+  val charSeq = ".*".r
+  val nonScenarioCharSeq = "\\w*[^(Scenario:)].*".r
+
+  def feature: Parser[Any] = "Feature:" ~ title ~ opt(description) ~ rep(scenario)
+
+  def scenario: Parser[Any] = "Scenario:" ~ title ~ opt(given) ~ opt(when) ~ opt(then)
+
+  def given: Parser[Any] = "Given:" ~ step ~ rep(and)
+
+  def when: Parser[Any] = "When:" ~ step ~ rep(and)
+
+  def then: Parser[Any] = "Then:" ~ step ~ rep(and)
+
+  def and: Parser[Any] = ("And:" | "But:") ~ step
+
+  def description: Parser[Any] = rep(nonScenarioCharSeq)
+
+  def title: Parser[Any] = charSeq
+
+  def step: Parser[Any] = charSeq
+}
